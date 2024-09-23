3619ee86dec190d5e41644c2ccb807c42e452183
esromtebebu <111666496+esromtebebu@users.noreply.github.com>
2024-09-23 10:49:33 +0200

    added render db

--- gasp_server/config.py
+++ gasp_server/config.py
@@ -11,17 +11,17 @@ class DevConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    DBNAME = 'gasp'
-    DBUSER = 'gasp'
-    DBPASSWORD = 'gasp.'
-    DBHOST = 'localhost'
+    DBNAME = 'agape'
+    DBUSER = 'eftebebu'
+    DBPASSWORD = 'xs5KGVeHNpQjQygRk2camXRKi7cSi7Xs'
+    DBHOST = 'dpg-croilh9u0jms73c9mhhg-a.frankfurt-postgres.render.com'
 
 class ProdConfig(BaseConfig):
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     LANGUAGES = ['en', 'fr']
-    DBNAME = 'gasp'
-    DBUSER = 'gasp'
-    DBPASSWORD = 'gasp.'
-    DBHOST = 'localhost'
+    DBNAME = 'agape'
+    DBUSER = 'eftebebu'
+    DBPASSWORD = 'xs5KGVeHNpQjQygRk2camXRKi7cSi7Xs'
+    DBHOST = 'dpg-croilh9u0jms73c9mhhg-a.frankfurt-postgres.render.com'
